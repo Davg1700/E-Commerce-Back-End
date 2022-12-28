2620a4f5f92c067ae1e1fc16a134acbce557ffce
Davg1700 <davg1700@gmail.com>
2022-12-27 23:28:35 -0700

    bug fixes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,4 +16,4 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
-})
+});
